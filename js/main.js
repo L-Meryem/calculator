f80264f69a6c255fb6f8afaa7b98ee9c2a41f4e9
Meryem L. <82731366+L-Meryem@users.noreply.github.com>
2025-09-14 19:36:09 -0400

    Add more values and numbers to click

--- js/main.js
+++ js/main.js
@@ -1,34 +1,60 @@
-/*
-Create a simple calculator that has two inputs and returns the result of +,-,/,* somewhere in the DOM
-*/
-
 const smurf = document.querySelectorAll('h1');
-smurf.forEach(h1 => h1.addEventListener('click', () => calculate(h1)));
+smurf.forEach(h1 => h1.addEventListener('click', () => calculator(h1)));
 
-function calculate(smurf) {
-    const numberOne = +document.querySelector('#numberOne').value;
-    const numberTwo = +document.querySelector('#numberTwo').value;
+function calculator(h1) {
     const result = document.querySelector('#result');
-    result.innerText = operation(smurf.id, numberOne, numberTwo);
 
+    if (h1.innerText === "C")
+        clear(result);
+    else if (h1.innerText === "=") {
+        const operationArray = stringToArray(result);
+        calculate(operationArray);
+    }else
+        result.value += h1.innerText;
 }
 
-function operation(operator,num1, num2){
-    switch(operator){
-        case 'add':
-            return  num1 + num2;
-            break;
-        case 'sub':
-            return  num1 - num2;
-            break;
-        case 'multi':
-            return  num1 * num2;
-            break;
-        case 'divid':
-            return  (num1 / num2).toFixed(8);
-            break;
 
+function clear(result) {
+    document.querySelector('#result').value = "";
+}
+
+function calculate(operationArray){
+      let calc = operationArray[0];
+
+        for (let i = 1; i < operationArray.length; i += 2) {
+            const operator = operationArray[i];
+            const number = operationArray[i + 1];
+
+            if (operator === '+') calc += number;
+            else if (operator === '-') calc -= number;
+            else if (operator === 'x') calc *= number;
+            else if (operator === '/') calc /= number;
+            else if (operator === '%') calc %= number;
+
+        document.querySelector('#result').value = calc;
+
+        }
+}
+
+
+ // 12+4-332 => [12, "+", 4, "-", 322] 
+function stringToArray(display) {
+
+    let myDisplay = display.value;
+    let myArr = [];
+    let num = "";
+
+    for (let i = 0; i < myDisplay.length; i++) {
+        if(!"+-x/%".includes(myDisplay[i])){
+            num = num + myDisplay[i];
+        } else {
+            myArr.push(+num);
+            myArr.push(myDisplay[i]);
+            num = "";
+        }
+        if (i == myDisplay.length - 1)
+            myArr.push(+num);
     }
 
+    return myArr;
 }
-
